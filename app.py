4ae68278ae8927ae912ebb2129c835628b31c55d
Danil <danilkaspirin@gmail.com>
2023-11-28 16:34:01 +1000

    finish lab

--- app.py
+++ app.py
@@ -14,15 +14,32 @@ def generate_empty_matrix(matrix_rank):
     return [[0 for _ in range(0, matrix_rank)] for _ in range(0, matrix_rank)]
 
 
-def get_matrix_from_request(matrix_name, matrix_rank):
+def get_matrix_from_request(matrix_name):
     """
-    Преобразует запрос в матрицу с учетом ее ранга
+    Преобразует запрос в матрицу
     :param matrix_name: название параметра для матрицы
-    :param matrix_rank: ранг матрицы
-    :return: матрицу
+    :return: матрица
     """
-    return [[int(request.args.get(f'{matrix_name}[{i}][{j}]')) for j in range(matrix_rank)] for i in
-            range(matrix_rank)]
+
+    def get_matrix_elem(i, j):
+        return request.values.get(f'{matrix_name}[{i}][{j}]')
+
+    matrix_result = []
+    row = 0
+    column = 0
+    current_element = get_matrix_elem(row, column)
+    while current_element is not None:
+        row_result = []
+        while current_element is not None:
+            row_result.append(int(current_element))
+            column += 1
+            current_element = get_matrix_elem(row, column)
+        matrix_result.append(row_result)
+        row += 1
+        column = 0
+        current_element = get_matrix_elem(row, column)
+
+    return matrix_result
 
 
 @app.get('/')
@@ -30,23 +47,24 @@ def index():
     return redirect(url_for('matrix'))
 
 
-@app.get('/matrix')
+@app.route('/matrix', methods=["GET", "POST"])
 def matrix():
-    matrix_rank = int(request.values.get("rank")) if request.values.get("rank") else 3
-
-    # если запрос посчитать
-    if request.values.get('action') == 'Посчитать':
+    if request.method == "POST":
         operation = request.values.get('operation')
-        matrix_a = get_matrix_from_request('a', matrix_rank)
-        matrix_b = get_matrix_from_request('b', matrix_rank)
+        matrix_a = get_matrix_from_request('a')
+        matrix_b = get_matrix_from_request('b')
         result = service_operations[operation](matrix_a, matrix_b)
+        matrix_rank = len(result)
     else:
+        matrix_rank = int(request.values.get("rank")) if request.values.get("rank") else 3
         matrix_a = generate_empty_matrix(matrix_rank)
         matrix_b = generate_empty_matrix(matrix_rank)
         result = generate_empty_matrix(matrix_rank)
+        operation = "sum"
 
     return render_template('matrix-rank.html',
                            rank=matrix_rank,
                            matrix_a=matrix_a,
                            matrix_b=matrix_b,
-                           result=result)
+                           result=result,
+                           operation=operation)
